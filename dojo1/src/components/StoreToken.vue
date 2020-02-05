aea2c9732985f17e578f40fcdee0837e206f44d6
Gustavo Freitas <gustavo.freitas@brasil317.com.br>
2020-02-05 16:56:23 -0300

    #3.3 Pires Styling

--- dojo1/src/components/StoreToken.vue
+++ dojo1/src/components/StoreToken.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="about">
     <div :value="token"></div>
-    <button @click="setToken(tokenInput)">setar token aqui !!!</button>
     <!-- <button @click="methodK">setar token aqui !!!</button> -->
-    <div>{{ this.stToken }} ---- {{this.tokenInput}}</div>
+    <div class="text">{{ this.stToken }}</div>
     <input type="text" v-model="tokenInput">
+        <div class="token_input">{{this.tokenInput}}</div>
 
+    <button class="btn" @click="setToken(tokenInput)">setar token aqui !!!</button>
   </div>
-
 </template>
+
+<style scoped>
+.about {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    }
+    .btn {
+        background-color: whitesmoke;
+        border: 1px solid lightgray;
+        border-radius: 5px;
+        height: 40px;
+        width: 200px;
+        font-size: 20px;
+        color: lightcoral;
+        margin: 10px;
+    }
+    .text {
+        font-size: 25px;
+        color: blue;
+        font-weight: 300px;
+        margin: 10px;
+    }
+    .text:hover {
+        color: coral;
+    }
+    .token_input {
+        margin: 25px;
+    }
+    input {
+        background-color: whitesmoke;
+        height: 40px;
+        width: 200px;
+        border: 3px dotted black;
+        margin: 20px;
+    }
+</style>
 
 <script>
 
--- dojo1/src/views/About.vue
+++ dojo1/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Sblim</h1>
     <StoreToken></StoreToken>
   </div>
 </template>
